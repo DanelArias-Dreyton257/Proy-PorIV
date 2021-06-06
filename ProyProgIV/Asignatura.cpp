83d63ae53ca4fc66587ac39c7d96586e40289ba0
DanelArias-Dreyton257 <danelarias7@gmail.com>
2021-06-06 10:01:59 +0200

    comentario asignaturas

--- ProyProgIV/Asignatura.cpp
+++ ProyProgIV/Asignatura.cpp
@@ -14,13 +14,24 @@
 #include <stdlib.h>
 using namespace std;
 
+//Crea el mapa de categorias con los nombres de cada asignatura
 unordered_map<string, string> Asignatura::mapaCategorias =
-		{ { "BD", "Base de datos" }, { "MT", "Matematicas" }, { "PR",
-				"Programacion" }, { "HS", "Historia" }, { "AD", "ADE" }, { "IN",
-				"Ingles" }, { "QC", "Computacion cuantica" },
-				{ "FI", "Fisica" }, { "BI", "Biologia" }, { "IN", "Ingles" }, {
-						"EA", "Estructura y Arquitectura de computadores" } };
+	  { { "BD", "Base de datos" },
+		{ "MT", "Matematicas" },
+		{ "PR", "Programacion" },
+		{ "HS", "Historia" },
+		{ "AD", "ADE" },
+		{ "IN", "Ingles" },
+		{ "QC", "Computacion cuantica" },
+		{ "FI", "Fisica" },
+		{ "BI", "Biologia" },
+		{ "IN", "Ingles" },
+		{ "EA", "Estructura y Arquitectura de computadores" } };
 
+/**
+ * Constructor de asignatura con el codigo de 2 letras de la asignatura en cuestion
+ * @param cat, char* con el codigo de 2 letras
+ */
 Asignatura::Asignatura(char *cat) {
 
 	//coger el valor del mapa
@@ -44,11 +55,17 @@ Asignatura::Asignatura(char *cat) {
 	}
 
 }
-
+/**
+ * Devuelve el numero de creditos de la asignatura
+ * @return NUM_CREDITOS
+ */
 const int Asignatura::getNumCreditos() {
 	return NUM_CREDITOS;
 }
 
+/**
+ * Constructor copia de la asignatura
+ */
 Asignatura::Asignatura(const Asignatura &other) :
 		Jugador(other) {
 	this->preguntas = new Pregunta[other.numPreguntas];
@@ -57,22 +74,33 @@ Asignatura::Asignatura(const Asignatura &other) :
 		this->preguntas[i] = duplicarPregunta(&other.preguntas[i]);
 	}
 }
-
+/**
+ * Destructor de la asignatura
+ */
 Asignatura::~Asignatura() {
 	for (int i = 0; i < numPreguntas; i++) {
 		freePregunta(preguntas + i);
 	}
 	delete[] preguntas;
 }
+
+/**
+ * Reduce la vida de la asinatura, segun el danyo
+ */
 void Asignatura::danyar() {
 	vida -= DANYO;
 }
 
+/**
+ * Saca por consola la asignatura
+ */
 void Asignatura::print() {
-	cout << nombre << "(" << NUM_CREDITOS << " creditos):" <<endl<<"\t" << vidaToString()
-			<< endl;
+	cout << nombre << "(" << NUM_CREDITOS << " creditos):" << endl << "\t"
+			<< vidaToString() << endl;
 }
-
+/**
+ * Saca por consola las preguntas de la asignatura
+ */
 void Asignatura::printPreguntas() {
 	cout << nombre << "(" << numPreguntas << "):" << endl;
 	for (int i = 0; i < numPreguntas; i++) {
@@ -81,10 +109,17 @@ void Asignatura::printPreguntas() {
 		cout << "\n";
 	}
 }
+
+/**
+ * Restaura la vida de la asignatura devolviendola a vidaMax
+ */
 void Asignatura::restaurar() {
 	this->vida = vidaMax;
 }
-
+/**
+ * Devuelve una pregunta elegida aleatoriamente
+ * @return puntero a la pregunta elegida
+ */
 Pregunta* Asignatura::getPreguntaRandom() {
 	int i = rand() % this->numPreguntas;
 	return this->preguntas + i;
